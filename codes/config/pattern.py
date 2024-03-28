172f825fd6292a0496d33176c00b15573ca04688
jongphago <jonghyun.d.kim@gmail.com>
2024-03-28 13:28:40 +0000

    feat (video.py): 영상에서 프레임을 추출하여 코코형식으로 변환하는 video class 추가

---
+++ codes/config/pattern.py
@@ -0,0 +1,12 @@
+import re
+
+
+def find(pattern, string):
+    return re.findall(pattern, string)[0]
+
+
+# AIHub dataset
+# 시나리오01/카메라11.avi
+#         ^~       ^~
+scenario_id_pattern = r"(?<=시나리오)\d{2}"
+camera_id_pattern = r"(?<=카메라)\d{2}"
---
+++ codes/dataset/video.py
@@ -0,0 +1,88 @@
+import os
+import sys
+
+sys.path.append("/home/jongphago/project/ultralytics")
+
+import argparse
+import subprocess
+from pathlib import Path
+
+import yaml
+import pandas as pd
+from box import Box
+
+from codes.config.pattern import find
+from codes.config.pattern import camera_id_pattern, scenario_id_pattern
+
+
+class Video:
+    def __init__(self, video_dir, task):
+        self.video_dir = video_dir
+        self.task = task
+
+
+class AIHub(Video):
+
+    def __init__(self, video_dir, task, cfg):
+        super().__init__(video_dir, task)
+        self.camera_id: str = find(camera_id_pattern, self.video_dir)  # 01
+        self.scenario_id: str = find(scenario_id_pattern, self.video_dir)  # 11
+        self.fps: int = cfg.fps  # 30
+        self.dirs(cfg)
+
+    def dirs(self, cfg):
+        self.path = Path(cfg.path)
+        self.raw = self.path / cfg.raw  # ex. aihub/sample/원천데이터/RGB
+        self.video_dir = (
+            self.path / self.raw / self.video_dir
+        )  # ex. 시나리오01/카메라07.avi
+        self.out = self.path / cfg.out  # ex. aihub/sample/frames
+        sub_dir = f"scenario{self.scenario_id}/camera{self.camera_id}"
+        self.frames_dir = self.out / sub_dir
+        self.img = self.path / cfg.img  # ex. aihub/sample/images
+        self.images_dir = self.img / self.task
+
+    def extract(self):
+        os.makedirs(self.frames_dir, exist_ok=True)
+        subprocess.run(
+            [
+                "ffmpeg",
+                *("-i", self.video_dir),
+                *("-r", str(self.fps)),
+                f"{self.frames_dir}/s{self.scenario_id}_c{self.camera_id}_f%04d.png",
+            ]
+        )
+
+    def link(self):
+        # link: Make symbolic link
+        os.makedirs(self.images_dir, exist_ok=True)
+        srcs = []
+        for directory, _, file_names in os.walk(self.frames_dir):
+            root = Path(directory)
+            for file_name in file_names:
+                srcs.append(root / file_name)
+        for src in srcs:
+            dst = self.images_dir / src.parts[-1]
+            if not os.path.exists(dst):
+                os.symlink(src, dst)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract and link AIHub data")
+    parser.add_argument("config_name", type=str, help="Name of the configuration file")
+
+    args = parser.parse_args()  # 명령행 인자 파싱하기
+
+    root = Path("/home/jongphago/project/ultralytics")
+    config_name = args.config_name  # aihub-sample, aihub-val
+    cfg_path = root / f"ultralytics/cfg/datasets/{config_name}.yaml"
+    with open(cfg_path, "r") as file:
+        cfg = Box(yaml.safe_load(file))
+
+    # root directory
+    dataset_dir = Path(cfg.path)
+    data_df = pd.read_csv(dataset_dir / cfg.csv)
+    for rows in data_df.itertuples():
+        aihub = AIHub(rows.video_dir, rows.task, cfg)
+        aihub.extract()
+        aihub.link()
