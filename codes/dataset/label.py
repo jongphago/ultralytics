a1c9cb2954c7b3da6bfb267a15ee6446a81e1111
jongphago <jonghyun.d.kim@gmail.com>
2024-04-02 11:49:39 +0000

    feat (label.py): AIHub 데이터셋의 라벨 파일에 대해 config 설정, 데이터 전처리 및 파일로 저장 프로세스를 수행하는 함수 모듈을 작성 - validation 데이터셋에 대해 테스트함

---
+++ codes/dataset/label.py
@@ -0,0 +1,205 @@
+import os
+import re
+import json
+import logging
+from tqdm import tqdm
+from pathlib import Path
+import yaml
+import pandas as pd
+from box import Box
+
+
+class Label:
+    def __init__(self, path):
+        self.path = path
+
+
+class AIHub(Label):
+    def __init__(
+        self,
+        scenario_id,
+        camera_id,
+        frame_id,
+        root="/home/jongphago/project/ultralytics/datasets/aihub/sample/라벨링데이터/",
+    ):
+        path = (
+            root
+            + f"시나리오{scenario_id:02d}/카메라{camera_id:02d}/"
+            + f"NIA_MTMDC_s{scenario_id:02d}_c{camera_id:02d}_"
+            + "pm_sunny_summer_"
+            + f"{frame_id:04d}.json"
+        )
+        super().__init__(path)
+
+    pass
+
+
+# config file (.yaml)
+def load_config_from_yaml(fp: str | Path) -> Box:
+    with open(fp) as f:
+        cfg = Box(yaml.safe_load(f))
+    return cfg
+
+
+def get_value_dict(cfg: Box) -> dict[str:int]:
+    value_dict = {value: key for key, value in cfg.names.items()}
+    return value_dict
+
+
+def get_videos(cfg: Box) -> map:
+    """AI is creating summary for get_videos
+
+    Args:
+        cfg (Box): directory information
+
+    Returns:
+        map: iterable pandas dataframe
+
+    Examples:
+    cfg = load_config_from_yaml(cfg_yaml_path)
+    rows = get_videos(cfg)
+    for row in rows:
+        row.task, row.video_dir
+    """
+    videos_df: pd.DataFrame = pd.read_csv(Path(cfg.path) / cfg.csv)
+    rows: map = videos_df.itertuples()
+    return rows
+
+
+def get_json_labels(cfg, sub_dir) -> list[Path]:
+    raw_label_path = Path(cfg.path) / cfg.lbl / sub_dir
+    label_paths = []
+    for dirs, _, file_names in os.walk(raw_label_path):
+        parent = Path(dirs)
+        for file_name in sorted(file_names):
+            label_paths.append(parent / file_name)
+    return label_paths
+
+
+def find(pattern, string):
+    return re.findall(pattern, string)[0]
+
+
+def raw2cvt(raw, cfg):
+    cvt_name = f"{raw.stem}.txt"
+    cvt = Path(cfg.path) / cfg.cvt / row.video_dir / cvt_name
+    return cvt
+
+
+def cvt2dst(cvt, cfg):
+    scenario_id_pattern = r"(?<=시나리오)\d{2}"
+    camera_id_pattern = r"(?<=카메라)\d{2}"
+    frame_id_pattern = r"_(\d{4})."
+
+    scenario_id = find(scenario_id_pattern, str(cvt))
+    camera_id = find(camera_id_pattern, str(cvt))
+    frame_id = find(frame_id_pattern, str(cvt))
+    dst_name = f"s{scenario_id}_c{camera_id}_f{frame_id}.txt"
+    dst = Path(cfg.path) / cfg.labels / dst_name
+    return dst
+
+
+def get_frames(lines: dict) -> pd.DataFrame:
+    # Convert to DataFrame [label, info]
+    # label = pd.DataFrame(label_dict, index=[0])
+    info = pd.DataFrame(lines["info"])
+
+    # Join [label, info] to `frames`
+    # frames = label.join(info)
+
+    return info
+
+
+def get_objects(lines: dict) -> pd.DataFrame | None:
+    def _flatten(x):
+        return pd.Series(x[0])
+
+    # Define objects dataframe
+    objects = pd.DataFrame(lines["objects"])
+
+    # Exception
+    if objects.loc[0, "label"] == "void":
+        return None
+
+    # Flatten [position, attributes] columns
+    position: pd.DataFrame = objects.position.apply(_flatten)
+    attributes: pd.DataFrame = objects.attributes.apply(_flatten)
+
+    # Join objects + [position, attributes]
+    objects = objects.join(position)
+    objects = objects.join(attributes)
+
+    # Drop [position, attributes] from objects dataframe
+    objects = objects.drop(["position", "attributes"], axis=1)
+
+    return objects
+
+
+def get_reorder(
+    objects: pd.DataFrame,
+    frames: pd.DataFrame,
+    value_dict: dict[str:int],
+) -> pd.DataFrame | None:
+    if objects is None:
+        return None
+    class_col = (
+        objects[["label"]]
+        .apply(lambda x: value_dict[x[0]], axis=1)
+        .to_frame(name="class")
+    )
+    norm_hor = objects[["x", "width"]] / frames.loc[0, "video_width"]
+    norm_ver = objects[["y", "height"]] / frames.loc[0, "video_heigth"]
+    joined = class_col.join([norm_hor, norm_ver])
+    reorder = joined[["class", "x", "y", "width", "height"]]
+    return reorder
+
+
+def convert(reorder, cvt_path) -> None:
+    if reorder is not None:
+        reorder.to_csv(cvt_path, header=False, index=False)
+    else:
+        with open(cvt_path, "w") as f:
+            f.write("")
+    return None
+
+
+if __name__ == "__main__":
+    # Logging
+    logging.basicConfig(level=logging.ERROR)
+    debug = logging.debug
+    info = logging.info
+    info(os.getcwd())
+
+    # Config
+    cfg_yaml_path = "ultralytics/cfg/datasets/aihub-val.yaml"
+    cfg: Box = load_config_from_yaml(cfg_yaml_path)
+    debug(cfg)
+    value_dict = get_value_dict(cfg)
+    debug(value_dict)
+
+    # Videos
+    rows = get_videos(cfg)
+    debug(rows)
+    for row in rows:
+        json_labels = get_json_labels(cfg, row.video_dir)
+        debug(row)
+
+        os.makedirs(Path(cfg.path) / cfg.cvt / row.video_dir, exist_ok=True)
+        os.makedirs(Path(cfg.path) / cfg.labels, exist_ok=True)
+
+        for raw in tqdm(json_labels):
+            cvt = raw2cvt(raw, cfg)
+            if cvt.exists():  # No overwrite
+                continue
+            dst = cvt2dst(cvt, cfg)
+            info(cvt)
+            info(dst)
+            with open(raw) as f:
+                lines = json.load(f)
+            objects = get_objects(lines)
+            frames = get_frames(lines)
+            reorder = get_reorder(objects, frames, value_dict)
+            info(objects)
+            convert(reorder, cvt)
+            if not os.path.exists(dst):
+                os.symlink(cvt, dst)
