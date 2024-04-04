f5d69ae636d9bc365db70f26025ffa33276d674f
jongphago <jonghyun.d.kim@gmail.com>
2024-04-04 06:07:41 +0000

    feat (label.py): xywh2ccwh 함수 추가 및 경로와 예외 처리 과정을 일부 수정함 - feat(xywh2ccwh): aihub label format을 coco label format으로 변환하는 함수를 추가 - refactor (cvt2dst): 경로를 생성하는 함수를 수정함 x -> val (하위 경로 'val' 추가, 이미지 경로와 통일하기 위함) - fix (main): 예외 처리 구조 수정 - symbolic 경로를 만든는 과정을 조건문 밖으로 이동함. cvt가 존재하여도 심볼릭 링크를 만드는 과정은 진행함

--- codes/dataset/label.py
+++ codes/dataset/label.py
@@ -86,7 +86,7 @@ def raw2cvt(raw, cfg):
     return cvt
 
 
-def cvt2dst(cvt, cfg):
+def cvt2dst(cvt, cfg, row):
     scenario_id_pattern = r"(?<=시나리오)\d{2}"
     camera_id_pattern = r"(?<=카메라)\d{2}"
     frame_id_pattern = r"_(\d{4})."
@@ -95,7 +95,7 @@ def cvt2dst(cvt, cfg):
     camera_id = find(camera_id_pattern, str(cvt))
     frame_id = find(frame_id_pattern, str(cvt))
     dst_name = f"s{scenario_id}_c{camera_id}_f{frame_id}.txt"
-    dst = Path(cfg.path) / cfg.labels / dst_name
+    dst = Path(cfg.path) / cfg.labels / row.task / dst_name
     return dst
 
 
@@ -140,6 +140,37 @@ def get_reorder(
     frames: pd.DataFrame,
     value_dict: dict[str:int],
 ) -> pd.DataFrame | None:
+    def xywh2ccwh(objects, shape=(1920, 1080), normalized=True):
+        img_width, img_height = shape
+        # top-left 수정
+        objects.width.where(objects.x > 0, objects.width + objects.x, inplace=True)
+        objects.height.where(objects.y > 0, objects.height + objects.y, inplace=True)
+        objects.x.where(objects.x > 0, 0, inplace=True)
+        objects.y.where(objects.y > 0, 0, inplace=True)
+        # bottom-right 수정
+        bottom_right_x = objects.x + objects.width
+        bottom_right_y = objects.y + objects.height
+        bottom_right_x.where(bottom_right_x < img_width, img_width, inplace=True)
+        bottom_right_y.where(bottom_right_y < img_height, img_height, inplace=True)
+        objects.width = bottom_right_x - objects.x
+        objects.height = bottom_right_y - objects.y
+        # center xy
+        x = objects.x + objects.width / 2
+        y = objects.y + objects.height / 2
+        # width, height
+        width = objects.width
+        height = objects.height
+        # normalized
+        if normalized:
+            x = x / img_width
+            y = y / img_height
+            width = objects.width / img_width
+            height = objects.height / img_height
+        # concatenate
+        x.name, y.name = "x", "y"
+        ccwh = pd.concat([x, y, width, height], axis=1)
+        return ccwh
+
     if objects is None:
         return None
     class_col = (
@@ -147,16 +178,15 @@ def get_reorder(
         .apply(lambda x: value_dict[x[0]], axis=1)
         .to_frame(name="class")
     )
-    norm_hor = objects[["x", "width"]] / frames.loc[0, "video_width"]
-    norm_ver = objects[["y", "height"]] / frames.loc[0, "video_heigth"]
-    joined = class_col.join([norm_hor, norm_ver])
+    ccwh = xywh2ccwh(objects)
+    joined = class_col.join(ccwh)
     reorder = joined[["class", "x", "y", "width", "height"]]
     return reorder
 
 
 def convert(reorder, cvt_path) -> None:
     if reorder is not None:
-        reorder.to_csv(cvt_path, header=False, index=False)
+        reorder.to_csv(cvt_path, sep=" ", header=False, index=False)
     else:
         with open(cvt_path, "w") as f:
             f.write("")
@@ -187,19 +217,19 @@ if __name__ == "__main__":
         os.makedirs(Path(cfg.path) / cfg.cvt / row.video_dir, exist_ok=True)
         os.makedirs(Path(cfg.path) / cfg.labels, exist_ok=True)
 
-        for raw in tqdm(json_labels):
-            cvt = raw2cvt(raw, cfg)
-            if cvt.exists():  # No overwrite
+        for index, raw in tqdm(enumerate(json_labels)):
+            if index % 23 != 0 and index != 7361:
                 continue
-            dst = cvt2dst(cvt, cfg)
-            info(cvt)
-            info(dst)
-            with open(raw) as f:
-                lines = json.load(f)
-            objects = get_objects(lines)
-            frames = get_frames(lines)
-            reorder = get_reorder(objects, frames, value_dict)
-            info(objects)
-            convert(reorder, cvt)
+            cvt = raw2cvt(raw, cfg)
+            dst = cvt2dst(cvt, cfg, row)
+            if not cvt.exists():  # No overwrite
+                with open(raw) as f:
+                    lines = json.load(f)
+                objects = get_objects(lines)
+                frames = get_frames(lines)
+                reorder = get_reorder(objects, frames, value_dict)
+                convert(reorder, cvt)
             if not os.path.exists(dst):
+                if not os.path.exists(dst.parent):
+                    os.makedirs(dst.parent)
                 os.symlink(cvt, dst)
