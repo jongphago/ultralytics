df2415fd40ffc01b92837140fb472ff02969b374
jongphago <jonghyun.d.kim@gmail.com>
2024-04-16 13:12:11 +0900

    feat (wnb.py): custom callback 함수 추가하여 wnb에 기록 feat (callback.py): Add plot p-r curve, plot *-conf curve

--- codes/config/wnb.py
+++ codes/config/wnb.py
@@ -1,6 +1,10 @@
+import os, sys
+
+sys.path.append(os.getcwd())
+import wandb
 from ultralytics import YOLO
 from wandb.integration.ultralytics import add_wandb_callback
-import wandb
+from codes.callback import callback
 
 # Step 1: Initialize a Weights & Biases run
 wandb.init(project="ultralytics", job_type="validation")
@@ -12,14 +16,20 @@ model = YOLO(f"{model_name}.pt")
 
 # Step 3: Add W&B Callback for Ultralytics
 add_wandb_callback(model, enable_model_checkpointing=True)
+model.add_callback("on_val_end", callback.on_val_end)
 
 # Step 4: Train and Fine-Tune the Model
 # model.train(project="ultralytics", data=dataset_name, epochs=5, imgsz=640)
 
 # Step 5: Validate the Model
-model.val(
+stats = model.val(
     data=dataset_name,
 )
+
+# Step 5.1: Logging Validation Statistics Results
+callback.plot_pr_curve(stats)
+for i in range(1, 4):  # 3 confidence curves: precision, recall, F1
+    callback.plot_confidence_curve(stats, i)
 
 # Step 6: Perform Inference and Log Results
 # model(["path/to/image1", "path/to/image2"])
